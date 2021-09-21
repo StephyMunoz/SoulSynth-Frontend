79556668939f683413fea075bf06c9d0ea2f63d2
Mateo Borja <mateo.borja@epn.edu.ec>
2021-09-20 23:16:07 -0500

    landing page

--- src/components/AuthMenu.js
+++ src/components/AuthMenu.js
@@ -3,37 +3,57 @@ import Link from "next/link";
 import ProfileMenu from "@/components/ProfileMenu";
 import { useAuth } from "@/contexts/auth";
 import { Skeleton } from "@material-ui/lab";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import styles from "@/styles/register.module.css";
 import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(() => ({
+  buttonGridDiv: {
+    padding: "5px"
+  },
+  registerPageButton: {
+    backgroundColor: "#40F113",
+    color: "black",
+    marginBottom: "5px",
+    borderRadius: "25px",
+    textAlign: "center"
+  },
+  goBackButton: {
+    backgroundColor: "#2A2A2A",
+    color: "#ffffff",
+    borderRadius: "25px",
+    textAlign: "center",
+    borderColor: "#ffffff"
+  }
+}));
 
 const AuthMenu = () => {
   const { user } = useAuth();
-
+  const classes = useStyles();
   if (user === null) {
     return <Skeleton variant="rect" width={100} height={30} />;
   }
 
   if (!user) {
     return (
-      <div>
+      <div className={classes.buttonGridDiv}>
         {/*<Link href="/login">*/}
         {/*  <MuiLink component="button" color="inherit">*/}
         {/*    Login*/}
         {/*  </MuiLink>*/}
         {/*</Link>*/}
-        <Grid item direction="row">
-          <Grid item direction="row" className={styles.RegisterPageButton}>
-            <Link href="/login">
-              <Button>Log In!</Button>
-            </Link>
-          </Grid>
-          <Grid item direction="row">
-            <div className={styles.NavText}>
-              <Button>Go back!</Button>
-            </div>
-          </Grid>
+        
+        <Grid item direction={'row'} className={classes.registerPageButton}>
+          <Link href="/login">
+            <Button>Log In!</Button>
+          </Link>
         </Grid>
+        <Grid item direction={'row'}>
+          <div>
+            <Button className={classes.goBackButton}>Go back!</Button>
+          </div>
+        </Grid>
+        
       </div>
     );
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,63 @@
-// import Head from 'next/head'
-// import Link from "next/link";
-// import Image from "next/image";
-// import Button from "@material-ui/core/Button";
-// import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
-import { CssBaseline } from "@material-ui/core";
+import Link from "next/link";
+import { CssBaseline, Grid, Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Image from "next/image";
+import headphones from "@/images/headphones.jpeg";
 
 const useStyles = makeStyles(() => ({
   background: {
-    minHeight: "100vh",
-    backgroundColor: "#2A2A2A",
-    backgroundSize: "cover",
+    backgroundColor: "#2A2A2A"
   },
+  gridForTwo: {
+    display: "flex",
+    justifyContent: "space-around",
+    paddingTop: "50px",
+    paddingBottom: "50px"
+  },
+  headphonesImage: {
+    borderRadius: "500px"
+  },
+  mainTitle: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingBottom: "50px"
+  },
+  mainText: {
+    color: "#ffffff",
+    width: "700px",
+    textAlign: "center"
+  },
+  sectionTitle: {
+    backgroundColor: "#c4c4c4",
+    textAlign: "center",
+    color: "#2A2A2A",
+    padding: "5px"
+  },
+  sectionBody: {
+    backgroundColor: "#2A2A2A",
+    padding: "30px 100px",
+    textAlign: "center",
+    color: "#ffffff"
+  },
+  loginButton: {
+    backgroundColor: "#40F113",
+    color: "black",
+    padding: "10px 50px",
+    borderRadius: "25px",
+    textAlign: "center",
+    fontSize: "2em"
+  },
+  signupButton: {
+    backgroundColor: "white",
+    color: "black",
+    padding: "10px 50px",
+    borderRadius: "25px",
+    textAlign: "center",
+    fontSize: "2em"
+  }
 }));
 
 export default function Home() {
@@ -20,6 +65,70 @@ export default function Home() {
   return (
     <div className={classes.background}>
       <CssBaseline />
+      <Grid container spacing={8} className={classes.gridForTwo}>
+        <Grid item direction={'column'}>
+          <Image src={headphones} width={540} height={500} className={classes.headphonesImage}/>
+        </Grid>
+        <Grid item direction={'column'}>
+          <Typography variant="h1" className={classes.mainTitle}>SoulSynth</Typography>
+          <Typography variant="h4" className={classes.mainText}>
+            This is a website that allows you to express how you feel and, based on that, get songs that fit
+            your current mood.
+          </Typography>
+        </Grid>
+      </Grid>
+      <div className={classes.sectionTitle}>
+        <Typography variant="h2">HOW DOES IT WORK?</Typography>
+      </div>
+      <div className={classes.sectionBody}>
+        <Typography variant="h4">        
+          <p>SoulSynth connects with Spotify to provide you with music that fits your current mood, 
+          the only thing that you need to do is sign in and tell us how you feel today, we’ll take 
+          care of the rest and in no time you will be listening to music in our player.</p>
+        </Typography>
+        <Typography variant="h4">
+          <p>Did we mention that you can delete a song if you don’t like it? Because you can! 
+          that way we can make sure that you will get only good music according to your taste.</p>
+        </Typography>
+        <Typography variant="h4">  
+          <p>Do you really like a playlist? Save it so that you can listen to it whenever you feel like it!</p>
+        </Typography>
+      </div>
+      <div className={classes.sectionTitle}>
+        <Typography variant="h2">WHAT DO YOU NEED?</Typography>
+      </div>
+      <div className={classes.sectionBody}>
+        <Typography variant="h4">        
+          <p>Not much! in fact, you only need a Spotify account to use our site, we need to use 
+          Spotify in order to deliver the best music directly to you without bothering you with 
+          pointless downloads, subscriptions or long loading times.</p>
+        </Typography>
+      </div>
+      <div className={classes.sectionTitle}>
+        <Typography variant="h2">GET STARTED!</Typography>
+      </div>
+      <div className={classes.sectionBody}>
+        <Typography variant="h4">        
+          <p>What are you waiting for? click on one of the buttons below or on top of the page to start!</p>
+        </Typography>
+        <Typography variant="h4">
+          <p>If you already linked your Spotify account, we will remeber you.
+          If it’s your first time signing up, be sure to allow us to access your Spotify account, 
+          don’t worry, we won’t touch anything!</p>
+        </Typography>
+        <Grid container spacing={8} className={classes.gridForTwo}>
+          <Grid item direction={'column'}>
+            <Link href="/login">
+              <Button className={classes.loginButton}>Log In!</Button>
+            </Link>
+          </Grid>
+          <Grid item direction={'column'}>
+            <Link href="/signIn">
+              <Button className={classes.signupButton}>Sign Up!</Button>
+            </Link>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
