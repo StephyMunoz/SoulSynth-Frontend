b42cc87a1fe575b4dcce0d56ff9124f1a93e20b2
stiven212 <stivfunk2000@hotmail.com>
2021-09-20 22:35:59 -0500

    buttons working

--- src/pages/songs/index.js
+++ src/pages/songs/index.js
@@ -1,3 +1,4 @@
+import {React, useState, useEffect, useRef} from "react";
 import styles from "@/styles/songs.module.css";
 import api from "@/api/api";
 import Image from "next/image";
@@ -11,17 +12,189 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
-import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import { Button } from "@material-ui/core";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import withAuth from "@/hocs/withAuth";
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import PauseRounded from '@mui/icons-material/PauseRounded';
+import PlayArrowRounded from '@mui/icons-material/PlayArrowRounded';
+import FastForwardRounded from '@mui/icons-material/FastForwardRounded';
+import FastRewindRounded from '@mui/icons-material/FastRewindRounded';
+import { CardMedia } from '@mui/material';
+import $ from "jquery";
+
+
+
+
+
+const Widget = styled('div')(({ theme }) => ({
+  padding: 16,
+  borderRadius: 16,
+  width: 343,
+  maxWidth: '100%',
+  margin: 'auto',
+  position: 'relative',
+  zIndex: 1,
+  backgroundColor:
+    theme.palette.mode === 'dark' ? 'rgba(0,0,0,0.6)' : 'rgba(255,255,255,0.4)',
+  backdropFilter: 'blur(40px)',
+}));
+
+const CoverImage = styled('div')({
+  width: 100,
+  height: 100,
+  objectFit: 'cover',
+  overflow: 'hidden',
+  flexShrink: 0,
+  borderRadius: 8,
+  backgroundColor: 'rgba(0,0,0,0.08)',
+  '& > img': {
+    width: '100%',
+  },
+});
+
+const TinyText = styled(Typography)({
+  fontSize: '0.75rem',
+  opacity: 0.38,
+  fontWeight: 500,
+  letterSpacing: 0.2,
+});
+
+
+
+
+
+
 
 const SongsPage = ({ songs }) => {
+
+  
+
+  const theme = useTheme();
+  const duration = 200; // seconds
+  const [position, setPosition] = useState(32);
+  const [paused, setPaused] = useState(false);
+  function formatDuration(value) {
+    const minute = Math.floor(value / 60);
+    const secondLeft = value - minute * 60;
+    return `${minute}:${secondLeft < 9 ? `0${secondLeft}` : secondLeft}`;
+  }
+
+  const mainIconColor = theme.palette.mode === 'dark' ? '#fff' : '#000';
+  
+
+  const [player, setPlayer] = useState(false);
+  const [songId, setsongId] = useState(undefined);
+  const [previousId, setPreviousId] = useState(undefined);
+  
   return (
     <div className={styles.songs}>
+      
+    <Box sx={{ width: '100%', overflow: 'hidden' }}>
+      <Widget>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <CoverImage>
+            
+          </CoverImage>
+          <Box sx={{ ml: 1.5, minWidth: 0 }}>
+            <Typography variant="caption" color="text.secondary" fontWeight={500}>
+              Jun Pulse
+            </Typography>
+            <Typography noWrap>
+              <b>คนเก่าเขาทำไว้ดี (Can&apos;t win)</b>
+            </Typography>
+            <Typography noWrap letterSpacing={-0.25}>
+              Chilling Sunday &mdash; คนเก่าเขาทำไว้ดี
+            </Typography>
+          </Box>
+        </Box>
+        <Slider
+          aria-label="time-indicator"
+          size="small"
+          value={position}
+          min={0}
+          step={1}
+          max={duration}
+          onChange={(_, value) => setPosition(value)}
+          sx={{
+            color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
+            height: 4,
+            '& .MuiSlider-thumb': {
+              width: 8,
+              height: 8,
+              transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
+              '&:before': {
+                boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
+              },
+              '&:hover, &.Mui-focusVisible': {
+                boxShadow: `0px 0px 0px 8px ${
+                  theme.palette.mode === 'dark'
+                    ? 'rgb(255 255 255 / 16%)'
+                    : 'rgb(0 0 0 / 16%)'
+                }`,
+              },
+              '&.Mui-active': {
+                width: 20,
+                height: 20,
+              },
+            },
+            '& .MuiSlider-rail': {
+              opacity: 0.28,
+            },
+          }}
+        />
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            mt: -2,
+          }}
+        >
+          <TinyText>{formatDuration(position)}</TinyText>
+          <TinyText>-{formatDuration(duration - position)}</TinyText>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            mt: -1,
+          }}
+        >
+          <IconButton aria-label="previous song">
+            <FastRewindRounded fontSize="large" htmlColor={mainIconColor} />
+          </IconButton>
+          <IconButton
+            aria-label={paused ? 'play' : 'pause'}
+            onClick={() => setPaused(!paused)}
+          >
+            {paused ? (
+              <PlayArrowRounded
+                sx={{ fontSize: '3rem' }}
+                htmlColor={mainIconColor}
+              />
+            ) : (
+              <PauseRounded sx={{ fontSize: '3rem' }} htmlColor={mainIconColor} />
+            )}
+          </IconButton>
+          <IconButton aria-label="next song">
+            <FastForwardRounded fontSize="large" htmlColor={mainIconColor} />
+          </IconButton>
+        </Box>
+        
+      </Widget>
+      
+    </Box>
+  
       <TableContainer component={Paper} className={styles.scrollv}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
+        <Table stickyHeader className={styles.table1} >
+          <TableHead className={styles.header}>
             <TableRow>
               <TableCell align="right"></TableCell>
               <TableCell align="right">Title</TableCell>
@@ -33,32 +206,50 @@ const SongsPage = ({ songs }) => {
           </TableHead>
           <TableBody styles={"max"}>
             {songs.map((song) => (
+              
               <TableRow key={song.name}>
                 <TableCell align="right">
-                  <Button>
-                    <PlayArrowIcon fontSize="large" className={styles.player} />
-                    <PauseIcon fontSize="large" className={styles.player} />
-                    <Image
-                      src={song.image}
-                      width={70}
-                      height={60}
-                      className={styles.image}
-                    />
+                  <Button id={song.id} onClick={() => {
+
+                   
+                      
+                      setsongId(song.id)
+                      if( !player && songId == song.id){
+                        $('#'+ song.id + 'play').attr('style', 'display:block !important')
+                        $('#'+ song.id + 'pause').attr('style', 'display:none !important')
+                        setPlayer(true)
+                        
+                      }
+                      
+                      if( player  && songId == song.id){
+                        $('#'+ song.id + 'pause').attr('style', 'display:block !important')
+                        $('#'+ song.id + 'play').attr('style', 'display:none !important')
+                        setPlayer(false)
+                      }
+                      
+                    
+                    
+                  }} >
+                    
+                    
+                    <PlayArrowIcon id={song.id + "play"} fontSize="large" value='false' className={styles.player2} /> 
+                    
+                    <PauseIcon id={song.id + "pause"} fontSize="large" value='true' className={styles.player1} />
+                    
+                    
+                  
                   </Button>
+                  
                 </TableCell>
                 <TableCell align="right">{song.name}</TableCell>
                 <TableCell align="right">{song.artist}</TableCell>
                 <TableCell align="right">{song.album}</TableCell>
                 <TableCell align="right">{song.genre}</TableCell>
                 <TableCell align="right">
-                  <div>
-                    <Button className={styles.button}>
-                      <AddCircleOutlineIcon />
-                    </Button>
-                    <Button className={styles.button}>
-                      <DeleteOutlineIcon />
-                    </Button>
-                  </div>
+              
+                <IconButton aria-label="AddCircleOutlineIcon">
+                    </IconButton>
+                  
                 </TableCell>
               </TableRow>
             ))}
@@ -69,7 +260,8 @@ const SongsPage = ({ songs }) => {
   );
 };
 
-export default withAuth(SongsPage);
+
+export default (SongsPage);
 
 SongsPage.propTypes = {
   songs: PropTypes.array.isRequired,
