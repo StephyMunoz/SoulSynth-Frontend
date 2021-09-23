9a0fd6a026feec390b31c06f9d3fb09d6c1d697a
StephyMunoz <58042439+StephyMunoz@users.noreply.github.com>
2021-09-22 23:29:08 -0500

    Playlists

--- src/pages/playlists/[id].js
+++ src/pages/playlists/[id].js
@@ -15,54 +15,21 @@ import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import PauseIcon from "@material-ui/icons/Pause";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import Playlist from "@/api/playlist";
-import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Slider from "@mui/material/Slider";
-import IconButton from "@mui/material/IconButton";
-import PauseRounded from "@mui/icons-material/PauseRounded";
-import PlayArrowRounded from "@mui/icons-material/PlayArrowRounded";
-import FastForwardRounded from "@mui/icons-material/FastForwardRounded";
-import FastRewindRounded from "@mui/icons-material/FastRewindRounded";
 import $ from "jquery";
 import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from "@material-ui/core/styles";
 import styledC from "styled-components";
+import Grid from "@material-ui/core/Grid";
+import SpotifyPlayer from "react-spotify-player";
 
-const Widget = styled("div")(({ theme }) => ({
-  padding: 16,
-  borderRadius: 16,
-  width: 800,
-  maxWidth: "100%",
-  margin: "auto",
-  position: "relative",
-  zIndex: 1,
-  backgroundColor:
-    theme.palette.mode === "dark" ? "rgba(0,0,0,0.6)" : "rgba(255,255,255,0.4)",
-  backdropFilter: "blur(40px)",
-}));
+const size = {
+  width: "100%",
+  height: 300,
+};
 
-const CoverImage = styled("div")({
-  width: 100,
-  height: 100,
-  objectFit: "cover",
-  overflow: "hidden",
-  flexShrink: 0,
-  borderRadius: 8,
-  backgroundColor: "rgba(0,0,0,0.08)",
-  "& > img": {
-    width: "100%",
-  },
-});
-
-const TinyText = styled(Typography)({
-  fontSize: "1.5em",
-  opacity: 0.38,
-  fontWeight: 500,
-  letterSpacing: 0.2,
-});
 const style = {
   position: "absolute",
   top: "50%",
@@ -70,12 +37,10 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "#00ffb380",
-  // border: "2px solid #000",
-  // boxShadow: 24,
   pt: 2,
   px: 4,
   pb: 3,
-  textAlign:'center',
+  textAlign: "center",
   color: "#ffffff",
   alignItems: "center",
   justifyContent: "center",
@@ -113,15 +78,12 @@ const PlaylistUserPage = () => {
   const router = useRouter();
   const { id } = router.query;
   const classes = useStyles();
-  const theme = useTheme();
-  const duration = 200; // seconds
-  const [position, setPosition] = useState(32);
-  const [paused, setPaused] = useState(false);
   const { data, error } = useSWR(`/playlists/${id}/songs`, fetcher);
   const [player, setPlayer] = useState(false);
   const [songId, setsongId] = useState(undefined);
   const [songIdDelete, setSongIdDelete] = useState(undefined);
   const [open, setOpen] = useState(false);
+  const [songSelect, setSongSelect] = useState(null);
   const handleClose = () => setOpen(false);
 
   if (error) {
@@ -137,119 +99,22 @@ const PlaylistUserPage = () => {
     console.log("response", response);
     handleClose();
   };
-  function formatDuration(value) {
-    const minute = Math.floor(value / 60);
-    const secondLeft = value - minute * 60;
-    return `${minute}:${secondLeft < 9 ? `0${secondLeft}` : secondLeft}`;
-  }
   const handleOpen = (idS) => {
     setOpen(true);
     setSongIdDelete(idS);
   };
-
-  const mainIconColor = theme.palette.mode === "dark" ? "#fff" : "#000";
-
+  const handlePlay = (songItem) => {
+    setSongSelect(songItem);
+  };
   return (
     <div className={styles.songs}>
-      <Box sx={{ width: "100%", overflow: "hidden" }}>
-        <Widget sx={{ padding: "2em", margin: "1.5em auto" }}>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: "0.5em",
-            }}
-          >
-            <CoverImage sx={{ width: "15em", height: "15em" }}></CoverImage>
-          </Box>
-          <Slider
-            aria-label="time-indicator"
-            size="small"
-            value={position}
-            min={0}
-            step={1}
-            max={duration}
-            onChange={(_, value) => setPosition(value)}
-            sx={{
-              color:
-                theme.palette.mode === "dark" ? "#fff" : "rgba(0,0,0,0.87)",
-              height: 4,
-              "& .MuiSlider-thumb": {
-                width: 8,
-                height: 8,
-                transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
-                "&:before": {
-                  boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
-                },
-                "&:hover, &.Mui-focusVisible": {
-                  boxShadow: `0px 0px 0px 8px ${
-                    theme.palette.mode === "dark"
-                      ? "rgb(255 255 255 / 16%)"
-                      : "rgb(0 0 0 / 16%)"
-                  }`,
-                },
-                "&.Mui-active": {
-                  width: 20,
-                  height: 20,
-                },
-              },
-              "& .MuiSlider-rail": {
-                opacity: 0.28,
-              },
-            }}
-          />
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              mt: -2,
-            }}
-          >
-            <TinyText>{() => formatDuration(position)} </TinyText>
-            <TinyText>-{() => formatDuration(duration - position)}</TinyText>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              mt: -1,
-            }}
-          >
-            <IconButton aria-label="previous song">
-              <FastRewindRounded fontSize="large" htmlColor={mainIconColor} />
-            </IconButton>
-            <IconButton
-              aria-label={paused ? "play" : "pause"}
-              onClick={() => setPaused(!paused)}
-            >
-              {paused ? (
-                <PlayArrowRounded
-                  fontSize="large"
-                  sx={{
-                    margin: "0 1em",
-                  }}
-                  htmlColor={mainIconColor}
-                />
-              ) : (
-                <PauseRounded
-                  fontSize="large"
-                  sx={{
-                    margin: "0 1em",
-                  }}
-                  htmlColor={mainIconColor}
-                />
-              )}
-            </IconButton>
-            <IconButton aria-label="next song">
-              <FastForwardRounded fontSize="large" htmlColor={mainIconColor} />
-            </IconButton>
-          </Box>
-        </Widget>
-      </Box>
-
+      {songSelect && (
+        <Grid container>
+          <Grid item xs={12}>
+            <SpotifyPlayer uri={songSelect.link} size={size} />
+          </Grid>
+        </Grid>
+      )}
       <TableContainer component={Paper} className={styles.scrollv}>
         <Table stickyHeader className={styles.table1}>
           <TableHead>
@@ -314,6 +179,7 @@ const PlaylistUserPage = () => {
                       fontSize="large"
                       value="false"
                       className={styles.player2}
+                      onClick={() => handlePlay(song)}
                     />
 
                     <PauseIcon
